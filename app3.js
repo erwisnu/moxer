3fda7780a66294c2fda92104755ece4f9f9c119a
Dina Developer <29623837+erwisnu@users.noreply.github.com>
2025-07-09 07:04:51 +0700

    UPDATE

--- app3.js
+++ app3.js
@@ -4,11 +4,37 @@ const jam = document.getElementsByClassName('jam');
 const menit = document.getElementsByClassName('menit');
 const detik = document.getElementsByClassName('detik');
 
+let currentJam = '';
+
+const signalClass = document.getElementsByClassName('signal');
+const signalText = signalClass[0].textContent;
+const signalTextArray = signalText.split(/\r?\n/).map(line => line.trim()).filter(line => line.length > 0);
+
 let siap2 = '<span class="kuning">SIAP SIAP ENTRY !! KALAU RAGU SKIP AJA YA 🤝🤝!!</span>'
 let semoga = '<span class="hijau"> OKE SEMOGA MASUK ENTRY PERTAMA 🤑🤑!!</span>';
 let jaga = 'TETAP TENANG - JAGA PSIKOLOGI - JANGAN LUPA WEDE 👌';
 let gabung = 'GABUNG VIP GRUP GRATIS DM <span class="hijau">ANGGITAA88</span> ATAU CEK DI CHANNEL KANGOPIT 😍😍';
 let ngopi = 'NGOPI DULU ☕☕ - JANGAN LUPA MINUM AIR PUTIH 🥛🥛 - JAGA KESEHATAN 😘😘';
+
+function parseSignalText(signalTextArray, jamBerapa) {
+  console.log('Parsing signal for jam:', jamBerapa);
+  const jamSignal = signalTextArray.find(line => line.includes(jamBerapa));
+  if (jamSignal) {
+    const parts = jamSignal.split(' ');
+    if (parts.length >= 2) {
+      const signal = parts[1];
+      siap2 = `${signal == 'B' ? '<span class="hijau">SIAP SIAP ENTRY BUY!!</span>' : '<span class="pink">SIAP SIAP ENTRY  SELL!!</span>'} <span class="kuning">KALAU RAGU SKIP AJA YA 🤝🤝!!</span>`
+      semoga = `<span class="hijau"> OKE SEMOGA AMAN UNTUK ENTRY ${signal == 'B' ? 'BUY' : 'SELL'} DI OPEN PERTAMA TANPA KOMPEN 🤑🤑!!</span>`
+      return
+    }
+  }
+
+  siap2 = '<span class="kuning">SIAP SIAP ENTRY !! KALAU RAGU SKIP AJA YA 🤝🤝!!</span>'
+
+  console.log(siap2)
+  return;
+
+}
 
 
 
@@ -34,6 +60,15 @@ function updateJam() {
   }
   else {
     detik[0].classList.remove('kuning');
+  }
+
+  const jamText = `${hours}.${minutesPlus}`;
+  if (currentJam !== jamText) {
+    console.log('Current Jam:', jamText);
+    currentJam = jamText;
+
+    parseSignalText(signalTextArray, jamText);
+
   }
 
 
